6850d7ed2c3189b62f5d6c9ec354be9e476a94d5
Pedro <pedroasilva38@icloud.com>
2025-10-05 15:15:36 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const listaSorteio = document.getElementById('lista-sorteio')
+const listaAmigos = document.getElementById('lista-amigos')
+let amigos = []
+function adicionar() {
+    const nomeAmigo = document.getElementById('nome-amigo').value.trim()
+    
+    if (!nomeAmigo) {
+        return
+    }
+    
+    if (!(amigos.includes(nomeAmigo))) {
+        if (listaAmigos.textContent == '') {
+            listaAmigos.textContent = nomeAmigo
+        } else {
+            listaAmigos.textContent += `, ${nomeAmigo}`
+        }
+    }
+    amigos.push(nomeAmigo)
+    document.getElementById('nome-amigo').value = ''
+}
+
+function sortear() {
+    document.getElementById('lista-sorteio').textContent = ''
+    let amigosSorteados = []
+    let max = amigos.length
+    
+    if (max % 2) {
+        alert('O número de amigos deve ser par!')
+        return
+    }
+    
+    for (i=0;i<max;i++) {
+        let amigoSorteado = Math.floor(Math.random() * max)
+        while (amigosSorteados.includes(amigos[amigoSorteado])) {
+            amigoSorteado = Math.floor(Math.random() * max)
+        }
+        amigosSorteados.push(amigos[amigoSorteado])
+        listaSorteio.innerHTML += `${amigos[i]} -> ${amigos[amigoSorteado]}<br/>`
+
+    }
+}
+
+function reiniciar() {
+    document.getElementById('nome-amigo').value = ''
+    document.getElementById('lista-amigos').textContent = ''
+    document.getElementById('lista-sorteio').textContent = ''
+    amigos = []   
+}
